9c60abebeb932ee130a124220d275bff0f8e0e50
Mike Partelow <github@mikepartelow.com>
2024-02-19 22:16:23 -0800

    algo

--- cmd/operator/main.go
+++ cmd/operator/main.go
@@ -5,4 +5,19 @@ import "github.com/mikepartelow/bumper/pkg/logging"
 func main() {
 	logger := logging.Init()
 	logger.Warn("Start")
+
+	// - set up env:
+	//   - git repo to monitor
+	//   - bumper registry
+	//   - branch name template
+
+	// - fetch git repo
+
+	// - bump
+
+	// - if diffs
+	//   - branch
+	//   - push
+
+	// - exit
 }
